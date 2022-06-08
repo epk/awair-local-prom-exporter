da7e6a063787567c37ca2cb7c04f8c6d0e9dd5de
Aditya Sharma <git@adi.run>
2022-06-07 18:02:10 -0700

    Add all metrics

--- go.mod
+++ go.mod
@@ -1,10 +1,11 @@
-module github.com/ericvolp12/awair-local-prom-exporter
+module github.com/epk/awair-local-prom-exporter
 
 go 1.18
 
 require (
 	github.com/prometheus/client_golang v1.12.2
 	go.uber.org/zap v1.21.0
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 )
 
 require (
@@ -13,10 +14,11 @@ require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/prometheus/client_model v0.2.0 // indirect
-	github.com/prometheus/common v0.32.1 // indirect
+	github.com/prometheus/common v0.34.0 // indirect
 	github.com/prometheus/procfs v0.7.3 // indirect
-	go.uber.org/atomic v1.7.0 // indirect
-	go.uber.org/multierr v1.6.0 // indirect
-	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
-	google.golang.org/protobuf v1.26.0 // indirect
+	github.com/spf13/pflag v1.0.5
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
-	"flag"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"os/signal"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/spf13/pflag"
 	"go.uber.org/zap"
+	"golang.org/x/sync/errgroup"
 )
 
 type App struct {
@@ -19,154 +24,249 @@ type App struct {
 	ListenPort        uint64
 	AwairAddress      string
 	TimeBetweenChecks time.Duration
-	TempGauge         prometheus.Gauge
-	HumidityGauge     prometheus.Gauge
-	Co2Gauge          prometheus.Gauge
-	VOCGauge          prometheus.Gauge
-	PM25Gauge         prometheus.Gauge
-	Logger            *zap.SugaredLogger
+
+	Logger *zap.Logger
+
+	TempGauge                 prometheus.Gauge
+	HumidityGauge             prometheus.Gauge
+	Co2Gauge                  prometheus.Gauge
+	VOCGauge                  prometheus.Gauge
+	PM25Gauge                 prometheus.Gauge
+	ScoreGauge                prometheus.Gauge
+	DewPointGauge             prometheus.Gauge
+	AbsoluteHumidityGauge     prometheus.Gauge
+	Co2EstimateGauge          prometheus.Gauge
+	Co2EstimateBaselinesGauge prometheus.Gauge
+	VOCBaselineGauge          prometheus.Gauge
+	VOCH2RawGauge             prometheus.Gauge
+	VocEthanolRawGauge        prometheus.Gauge
+	Pm10EstimateGauge         prometheus.Gauge
 }
 
 type AwairStats struct {
-	Timestamp      time.Time `json:"timestamp"`
-	Score          int       `json:"score"`
-	DewPoint       float64   `json:"dew_point"`
-	Temp           float64   `json:"temp"`
-	Humid          float64   `json:"humid"`
-	AbsHumid       float64   `json:"abs_humid"`
-	Co2            int       `json:"co2"`
-	Co2Est         int       `json:"co2_est"`
-	Co2EstBaseline int       `json:"co2_est_baseline"`
-	Voc            int       `json:"voc"`
-	VocBaseline    int       `json:"voc_baseline"`
-	VocH2Raw       int       `json:"voc_h2_raw"`
-	VocEthanolRaw  int       `json:"voc_ethanol_raw"`
-	Pm25           int       `json:"pm25"`
-	Pm10Est        int       `json:"pm10_est"`
+	Timestamp time.Time `json:"timestamp"`
+
+	Score          int     `json:"score"`
+	DewPoint       float64 `json:"dew_point"`
+	Temp           float64 `json:"temp"`
+	Humid          float64 `json:"humid"`
+	AbsHumid       float64 `json:"abs_humid"`
+	Co2            int     `json:"co2"`
+	Co2Est         int     `json:"co2_est"`
+	Co2EstBaseline int     `json:"co2_est_baseline"`
+	Voc            int     `json:"voc"`
+	VocBaseline    int     `json:"voc_baseline"`
+	VocH2Raw       int     `json:"voc_h2_raw"`
+	VocEthanolRaw  int     `json:"voc_ethanol_raw"`
+	Pm25           int     `json:"pm25"`
+	Pm10Est        int     `json:"pm10_est"`
 }
 
 func main() {
+	_ctx, cancel := signal.NotifyContext(context.Background(), os.Kill, os.Interrupt)
+	defer cancel()
+	group, gctx := errgroup.WithContext(_ctx)
 
 	rawLogger, err := zap.NewProduction()
 	if err != nil {
 		panic(fmt.Sprintf("Failed to start logger: %+v", err))
 	}
 
-	sugaredLogger := rawLogger.Sugar()
-
 	app := App{
-		Logger: sugaredLogger,
+		Logger: rawLogger,
 	}
 
 	// Initialize Flags for configuration
-	listenAddress := flag.String("listen", "0.0.0.0", "Listen address")
-	listenPort := flag.Uint64("port", 2112, "Listen port number")
-	awairAddress := flag.String("awair_address", "http://localhost/air-data/latest", "Awair air-data URL")
-	pollFrequency := flag.String("poll_frequency", "30s", "Time (seconds) to wait between polling device")
-
-	flag.Parse()
-
-	app.ListenAddress = *listenAddress
-	app.ListenPort = *listenPort
-	app.AwairAddress = *awairAddress
-
-	// Parse time duration from poll frequency flag
-	app.TimeBetweenChecks, err = time.ParseDuration(*pollFrequency)
-	if err != nil {
-		app.Logger.Fatalf("Couldn't parse duration from poll_frequency (%+v): %+v", *pollFrequency, err)
-	}
+	pflag.StringVar(&app.ListenAddress, "listen", "0.0.0.0", "Listen address")
+	pflag.Uint64Var(&app.ListenPort, "port", 2112, "Listen port number")
+	pflag.StringVar(&app.AwairAddress, "awair-address", "http://localhost/air-data/latest", "Awair air-data URL")
+	pflag.DurationVar(&app.TimeBetweenChecks, "poll-frequency", time.Second*30, "Duration to wait between polling device")
+	pflag.Parse()
 
 	// Initialize the Prometheus Gauges
 	app.initializeGauges()
-
-	// Start the metrics recording goroutine
-	app.recordMetrics()
-
-	// Register the metrics handler
 	http.Handle("/metrics", promhttp.Handler())
+
+	group.Go(func() error {
+		app.recordMetrics(gctx)
+		return nil
+	})
 
 	listenString := fmt.Sprintf("%s:%d", app.ListenAddress, app.ListenPort)
 
-	app.Logger.Infof("Awair Poller started on (%+v) polling Awair Device at (%+v) every (%+v)", listenString, app.AwairAddress, app.TimeBetweenChecks)
-
-	err = http.ListenAndServe(listenString, nil)
-	if err != nil {
-		app.Logger.Fatalf("Failed to start server: %+v", err)
+	server := &http.Server{
+		Addr: listenString,
 	}
+
+	group.Go(func() error {
+		app.Logger.Info("Starting server", zap.String("listen", listenString))
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			return fmt.Errorf("failed to start server: %+v", err)
+		}
+		return nil
+	})
+
+	app.Logger.Info("Awair Poller started", zap.String("listen_address", listenString), zap.String("awair_address", app.AwairAddress), zap.Duration("poll_frequency", app.TimeBetweenChecks))
+
+	<-_ctx.Done()
+	app.Logger.Info("Shutting down")
+
+	cancelCtx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	if err := server.Shutdown(cancelCtx); err != nil {
+		app.Logger.Error("Error shutting down server", zap.Error(err))
+	}
+
+	if err := group.Wait(); err != nil {
+		app.Logger.Error("Error shutting down", zap.Error(err))
+	}
+
+	app.Logger.Info("Shutdown complete")
 }
 
 func (app *App) initializeGauges() {
-	tempGauge := promauto.NewGauge(prometheus.GaugeOpts{
+	app.TempGauge = promauto.NewGauge(prometheus.GaugeOpts{
 		Namespace: "awair",
 		Subsystem: "climate",
 		Name:      "temp_c",
-		Help:      "The current temperature in C",
+		Help:      "Dry bulb temperature (ºC)",
 	})
 
-	humidityGauge := promauto.NewGauge(prometheus.GaugeOpts{
+	app.HumidityGauge = promauto.NewGauge(prometheus.GaugeOpts{
 		Namespace: "awair",
 		Subsystem: "climate",
 		Name:      "relative_humidity",
-		Help:      "The current % relative humidity",
+		Help:      "Relative Humidity (%)",
 	})
 
-	co2Gauge := promauto.NewGauge(prometheus.GaugeOpts{
+	app.Co2Gauge = promauto.NewGauge(prometheus.GaugeOpts{
 		Namespace: "awair",
 		Subsystem: "climate",
 		Name:      "co2_ppm",
-		Help:      "The current C02 PPM",
+		Help:      "Carbon Dioxide (ppm)",
 	})
 
-	vocGauge := promauto.NewGauge(prometheus.GaugeOpts{
+	app.VOCGauge = promauto.NewGauge(prometheus.GaugeOpts{
 		Namespace: "awair",
 		Subsystem: "climate",
 		Name:      "voc_ppb",
-		Help:      "The current Volatile Organic Compound reading in parts per billion",
+		Help:      "Total Volatile Organic Compounds (ppb)",
 	})
 
-	pm25Gauge := promauto.NewGauge(prometheus.GaugeOpts{
+	app.PM25Gauge = promauto.NewGauge(prometheus.GaugeOpts{
 		Namespace: "awair",
 		Subsystem: "climate",
 		Name:      "pm25_ug_m3",
-		Help:      "The current concentration of 2.5 micron particles in micrograms per meter cubed",
+		Help:      "Particulate matter less than 2.5 microns in diameter (µg/m³)",
 	})
 
-	app.TempGauge = tempGauge
-	app.HumidityGauge = humidityGauge
-	app.Co2Gauge = co2Gauge
-	app.VOCGauge = vocGauge
-	app.PM25Gauge = pm25Gauge
+	app.ScoreGauge = promauto.NewGauge(prometheus.GaugeOpts{
+		Namespace: "awair",
+		Subsystem: "climate",
+		Name:      "score",
+		Help:      "Awair Score (0-100)",
+	})
+
+	app.DewPointGauge = promauto.NewGauge(prometheus.GaugeOpts{
+		Namespace: "awair",
+		Subsystem: "climate",
+		Name:      "dew_point_c",
+		Help:      "The temperature at which water will condense and form into dew (ºC)",
+	})
+
+	app.AbsoluteHumidityGauge = promauto.NewGauge(prometheus.GaugeOpts{
+		Namespace: "awair",
+		Subsystem: "climate",
+		Name:      "absolute_humidity",
+		Help:      "Absolute Humidity (g/m³)",
+	})
+
+	app.Co2EstimateGauge = promauto.NewGauge(prometheus.GaugeOpts{
+		Namespace: "awair",
+		Subsystem: "climate",
+		Name:      "co2_estimate",
+		Help:      "Estimated Carbon Dioxide (ppm - calculated by the TVOC sensor)",
+	})
+
+	app.Co2EstimateBaselinesGauge = promauto.NewGauge(prometheus.GaugeOpts{
+		Namespace: "awair",
+		Subsystem: "climate",
+		Name:      "co2_estimate_baselines",
+		Help:      "A unitless value that represents the baseline from which the TVOC sensor partially derives its estimated (e)CO₂output.",
+	})
+
+	app.VOCBaselineGauge = promauto.NewGauge(prometheus.GaugeOpts{
+		Namespace: "awair",
+		Subsystem: "climate",
+		Name:      "voc_baseline",
+		Help:      "A unitless value that represents the baseline from which the TVOC sensor partially derives its TVOC output.",
+	})
+
+	app.VOCH2RawGauge = promauto.NewGauge(prometheus.GaugeOpts{
+		Namespace: "awair",
+		Subsystem: "climate",
+		Name:      "voc_h2_raw",
+		Help:      "A unitless value that represents the Hydrogen gas signal from which the TVOC sensor partially derives its TVOC output.",
+	})
+
+	app.VocEthanolRawGauge = promauto.NewGauge(prometheus.GaugeOpts{
+		Namespace: "awair",
+		Subsystem: "climate",
+		Name:      "voc_ethanol_raw",
+		Help:      "A unitless value that represents the Ethanol gas signal from which the TVOC sensor partially derives its TVOC output.",
+	})
+
+	app.Pm10EstimateGauge = promauto.NewGauge(prometheus.GaugeOpts{
+		Namespace: "awair",
+		Subsystem: "climate",
+		Name:      "pm10_estimate",
+		Help:      "Estimated particulate matter less than 10 microns in diameter (µg/m³ - calculated by the PM2.5 sensor)",
+	})
 }
 
-func (app *App) recordMetrics() {
-	go func() {
-		for {
-			app.getAwairData()
-			time.Sleep(app.TimeBetweenChecks)
+func (app *App) recordMetrics(ctx context.Context) {
+	ticker := time.NewTicker(app.TimeBetweenChecks)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			app.getAwairData(ctx)
+		case <-ctx.Done():
+			return
 		}
-	}()
+	}
 }
 
-func (app *App) getAwairData() {
-	resp, err := http.Get(app.AwairAddress)
+func (app *App) getAwairData(ctx context.Context) error {
+	ctx, cancel := context.WithTimeout(ctx, time.Second)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, app.AwairAddress, nil)
 	if err != nil {
-		app.Logger.Errorf("Failed to GET from configured Awair Address (%+v): %+v", app.AwairAddress, err)
-		return
+		app.Logger.Error("Error creating request", zap.Error(err))
+		return err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		app.Logger.Error("Error getting data from awair", zap.Error(err))
+		return err
 	}
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		app.Logger.Errorf("Failed to read body from Awair GET response: %+v", err)
-		return
+		app.Logger.Error("Error reading response body", zap.Error(err))
+		return err
 	}
 
 	awairStats := AwairStats{}
-
 	err = json.Unmarshal(body, &awairStats)
 	if err != nil {
-		app.Logger.Errorf("Failed to unmarshal Awair GET body into JSON: %+v", err)
-		return
+		app.Logger.Error("Error unmarshalling response body", zap.Error(err))
+		return err
 	}
 
 	app.TempGauge.Set(awairStats.Temp)
@@ -174,4 +274,17 @@ func (app *App) getAwairData() {
 	app.Co2Gauge.Set(float64(awairStats.Co2))
 	app.VOCGauge.Set(float64(awairStats.Voc))
 	app.PM25Gauge.Set(float64(awairStats.Pm25))
+	app.ScoreGauge.Set(float64(awairStats.Score))
+	app.DewPointGauge.Set(awairStats.DewPoint)
+	app.AbsoluteHumidityGauge.Set(awairStats.AbsHumid)
+	app.Co2EstimateGauge.Set(float64(awairStats.Co2Est))
+	app.Co2EstimateBaselinesGauge.Set(float64(awairStats.Co2EstBaseline))
+	app.VOCBaselineGauge.Set(float64(awairStats.VocBaseline))
+	app.VOCH2RawGauge.Set(float64(awairStats.VocH2Raw))
+	app.VocEthanolRawGauge.Set(float64(awairStats.VocEthanolRaw))
+	app.Pm10EstimateGauge.Set(float64(awairStats.Pm10Est))
+
+	app.Logger.Info("Successfully recorded metrics from Awair", zap.Any("metrics", awairStats))
+
+	return nil
 }
